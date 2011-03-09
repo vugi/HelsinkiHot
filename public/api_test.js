8072407fd43139497e3f7380d8b6dcbf869696aa
Antti Vuorela <antti.vuorela@iki.fi>
2011-03-10 00:11:35 +0200

    Fetch venue data from Foursquare and show as a list

--- public/api_test.js
+++ public/api_test.js
@@ -1,13 +1,23 @@
 /**
- * @author mikko
+ * Loads data from API and displays it.
  */
 
  $(document).ready(function(){
 	$.ajax({
-	type: "GET",
-	url: "api/",
-   	success: function(msg){
-     		$('body').append('<div>Server response from url <b>api/</b> <br /><code>' + msg + '</code></div>');
-   		}
+		type: "GET",
+		url: "api/venues",
+   		success: function(jsonData){
+			console.log(jsonData);
+			showData(jsonData);
+		}
  	});
  });
+
+function showData(jsonData){
+	var list = $('<ol>').appendTo('body');
+	$(jsonData.items).each(function(i,item){
+		console.log(item);
+		$('<li>').html(item.name+' '+item.hereNow.count+'/'+item.stats.checkinsCount).appendTo(list);
+	})
+	
+}
