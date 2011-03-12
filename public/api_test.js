359907f7e2dc165de15d095006eaa769da838b47
Antti Vuorela <antti.vuorela@iki.fi>
2011-03-12 17:34:55 +0200

    Flickr prototype

--- public/api_test.js
+++ public/api_test.js
@@ -3,24 +3,53 @@
  */
 
 var map;
+var flickrUrl = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=fdcac6bab07bf694be5d47005ba304ee&lat=60.170833&lon=24.9375&sort=date-taken-desc&radius=10&extras=date_taken,geo,url_s,url_sq&min_taken_date=2011-03-12&max_taken_date=2011-03-13&format=json&jsoncallback=?";
 
 $(document).ready(function(){
 	initializeMap();
+	
+	/* Foursquare */
 	$.ajax({
 		type: "GET",
 		url: "api/venues",
    		success: function(jsonData){
 			console.log(jsonData);
-			showData(jsonData);
+			showForsquareData(jsonData);
 		}
+	});
+	
+	/* Flickr */
+	$.getJSON(flickrUrl,function(data){
+		console.log(data);
+		showFlickrData(data);
 	});
 });
 
-function showData(jsonData){
+function showFlickrData(jsonData){
+	$(jsonData.photos.photo).each(function(i,item){			
+		// Google maps Info Window
+		var infowindow = new google.maps.InfoWindow({
+		    content: '<img src="'+item.url_s+'" />'
+		});
+		var marker = new google.maps.Marker({
+			position: new google.maps.LatLng(item.latitude,item.longitude), 
+			map: map, 
+			title: item.title,
+			icon: item.url_sq,
+			animation: google.maps.Animation.DROP
+		});
+		google.maps.event.addListener(marker, 'mouseover', function() {
+		  infowindow.open(map,marker);
+		});
+		google.maps.event.addListener(marker, 'mouseout', function() {
+		  infowindow.close();
+		});
+	});
+}
+
+function showForsquareData(jsonData){
 	var list = $('<ol>').appendTo('body');
-	$(jsonData).each(function(i,item){
-		console.log(item);
-		
+	$(jsonData).each(function(i,item){		
 		// List item
 		$('<li>').html(item.name+' '+item.hereNow.count+'/'+item.stats.checkinsCount).appendTo(list);
 		
