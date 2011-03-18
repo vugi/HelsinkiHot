1cd9aac20bb193e46588c764c98b9124952c9600
Antti Vuorela <antti.vuorela@iki.fi>
2011-03-18 14:47:15 +0200

    MongoDB test

--- app.js
+++ app.js
@@ -6,6 +6,31 @@ var https = require('https');
 var host = "api.foursquare.com";
 var path = "/v2/venues/trending?ll=60.170833,24.9375&limit=50&radius=1000000&client_id=LTEVQYSCQZZQKSPR1XAI4B0SAUD44AN4JKNURCL1ZFJ1IBDZ&client_secret=TL2ALQWU4VV5J5R5BCH3Z53EDFOU5KLSOIFZSJGLOSK4NGH1";
 
+var mongodb = require('mongodb');
+var db = new mongodb.Db('testdb');
+
+db.open(function(){
+	console.log("Mongo open!");	
+	
+	db.collection("testcollection", function(err,collection){
+		console.log("Collection available");
+		
+		collection.insert({test:"value"},function(){
+			console.log("Inserted to collection");
+			
+			collection.find(function(err, cursor) {
+				console.log("Read from collection");
+				cursor.each(function(err, item) {
+					if(item != null) {//null signifies end of iterator
+						console.log(item);
+					}
+				});                    
+			});	
+		});
+	});
+});
+
+
 // Middleware configurations
 app.configure(function(){
   app.use(express.staticProvider(__dirname + '/public'));
