a5fdc0d8a7dd83f3b4c5753ab90ffbe295ff5c83
Mikko Koski <mikkokos@gmail.com>
2011-03-22 23:18:25 +0200

    Decolorize

--- public/Heatmap.js
+++ public/Heatmap.js
@@ -81,12 +81,80 @@ Heatmap.prototype.colorize = function(x, y, x2) {
     ctx.putImageData(image, x, y);
 };
 
-Heatmap.prototype.decolorize = function(point) {
-	
+Heatmap.prototype.decolorize = function() {
+	var width = this._width;
+	var height = this._height;
+	var ctx = this._ctx;
+	var x = 0;
+	var y = 0;
+
+    var image = ctx.getImageData(x, y, width, height), // some performance tweaks
+    imageData = image.data, length = imageData.length;
+    // loop thru the area
+    for (var i = 3; i < length; i += 4) {
+    
+        var r = 0, g = 0, b = 0, tmp = 0,    // [0] -> r, [1] -> g, [2] -> b, [3] -> alpha
+        alpha = imageData[i] - 10;
+        
+		/*
+        // coloring depending on the current alpha value
+        if (alpha <= 255 && alpha >= 235) {
+            tmp = 255 - alpha;
+            r = 255 - tmp;
+            g = tmp * 12;
+        }
+        else 
+            if (alpha <= 234 && alpha >= 200) {
+                tmp = 234 - alpha;
+                r = 255 - (tmp * 8);
+                g = 255;
+            }
+            else 
+                if (alpha <= 199 && alpha >= 150) {
+                    tmp = 199 - alpha;
+                    g = 255;
+                    b = tmp * 5;
+                }
+                else 
+                    if (alpha <= 149 && alpha >= 100) {
+                        tmp = 149 - alpha;
+                        g = 255 - (tmp * 5);
+                        b = 255;
+                    }
+                    else 
+                        b = 255;
+        // we ve started with i=3
+        // set the new r, g and b values
+        imageData[i - 3] = r;
+        imageData[i - 2] = g;
+        imageData[i - 1] = b;
+        
+        */
+		
+		if(alpha > 0) {
+			imageData[i] = alpha;
+		} else {
+			imageData[i] = 0;
+		}
+        
+    }
+    // the rgb data manipulation didn't affect the ImageData object(defined on the top)
+    // after the manipulation process we have to set the manipulated data to the ImageData object
+    image.data = imageData;
+    ctx.putImageData(image, x, y);
 };
+
+Heatmap.prototype.fade = function(alphaChange) {
+	
+}
 
 Heatmap.prototype.mouseClickHandler = function(ev, that){
 	console.log('Heatmap mouse click');
+	
+	var colorize = !(ev.altKey);
+	if(!colorize){
+		return that.decolorize();
+	}
 	
 	// at first we have to get the x and y values of the user's mouse position
     var x, y;
@@ -106,20 +174,15 @@ Heatmap.prototype.mouseClickHandler = function(ev, that){
     var r1 = that._radius1;
     var r2 = that._radius2;
 	var ctx = that._ctx;
-	var colorize = !(ev.altKey);
     
     //console.log("x: "+x+"; y:" +y);
     // create a radial gradient with the defined parameters. we want to draw an alphamap
     var rgr = ctx.createRadialGradient(x, y, r1, x, y, r2);
     // the center of the radial gradient has .1 alpha value
-    if (colorize) {
-		rgr.addColorStop(0, 'rgba(0,0,0,0.1)');
-		rgr.addColorStop(1, 'rgba(0,0,0,0)');
-	} else {
-		rgr.addColorStop(0, 'rgba(255,255,255,0.1)');
-		rgr.addColorStop(1, 'rgba(255,255,255,0)');		
-	}
-    // drawing the gradient
+	rgr.addColorStop(0, 'rgba(0,0,0,0.1)');
+	rgr.addColorStop(1, 'rgba(0,0,0,0)');
+    
+	// drawing the gradient
     ctx.fillStyle = rgr;
     ctx.fillRect(x - r2, y - r2, 2 * r2, 2 * r2);
 
