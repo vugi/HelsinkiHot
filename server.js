81cf473ef4148b7fcf5779ea1f106e863c37bec3
Pyry Kröger <pyry@krogerit.net>
2011-03-26 13:00:15 +0200

    added simple interface for adding events to db

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ var datamodel = {
 
     return instance;
   },
-  init: function() {
+  init: function(addData) {
     
     datamodel.connect();
     
@@ -61,6 +61,8 @@ var datamodel = {
       identifier  : ObjectId,
       name        : String,
       address     : String,
+      latitude    : Number,
+      longitude   : Number,
       events      : [Event]
     });
     
@@ -71,6 +73,10 @@ var datamodel = {
     
     datamodel.models.Event = mongoose.model('Event');
     datamodel.models.Venue = mongoose.model('Venue');
+    
+    if (addData) {
+      datamodel.insertSampleData();
+    }
     
   },
   insertSampleData: function() {
@@ -90,14 +96,92 @@ var datamodel = {
     
     v1.save(function() {console.log('Saved v1')});
     v2.save(function() {console.log('Saved v2')});
+  },
+  
+  /**
+  * Add events to database
+  * @param events array that contains the events, or a single event info in a hash
+  */
+  addEvents: function(data, callback) {
+    if (typeof data == "array") {
+      for (var i in data) {
+        datamodel.addEvent(events[i]);
+      }
+    } else {
+      datamodel.addEvent(data);
+    }
+    
+    if (typeof callback === "function")
+    callback(true);
+  },
+  addEvent: function(data, callback) {
+    console.log('!!!!');
+    datamodel.getVenues({name: data.name, address: data.address}, function(venues) {
+      var venue;
+      console.log(venues);
+      if (venues.length == 1) {
+        console.log("Venue " + venues[0].name + " found");
+        venue = venues[0];
+      } else if (venues.length > 1) {
+        console.warn("Found multiple venues with name: " + data.name + ", address: " 
+        + data.address + ", choosing the first one.");
+        venue = venues[0];
+      } else {
+        console.log("Creating a new venue with data " + data);
+        venue = new datamodel.models.Venue();
+        venue.name = data.name;
+        venue.address = data.address; 
+        //TODO: try with data embedded into constructor call
+      }
+
+      for (var i in data.events) {
+        var ev = data.events[i];
+        console.log('Adding event ' + ev + ' to the event');
+        venue.events.push(ev);
+      }
+      
+      console.log(typeof callback);
+      if (typeof callback === "function") {
+        callback(true);
+      }
+      venue.save(function() {console.log('Saved the venue');});
+    });
+  },
+  getVenues: function(data, callback) {
+    datamodel.models.Venue.find(data, function(err, venuedata) {
+      callback(venuedata);
+    });
   }
 }
 
 // init the data model
-datamodel.init();
+datamodel.init(false);
 
 app.get('/api', function(req, res){
 	res.send('hello world from api!');	
+});
+
+app.get('/api/venues/add', function(req, res) {
+  datamodel.addEvents([{name:"Kauniainen", address: "Kauniaistentie 10", 
+    events: [
+      {time: new Date(), type: 'checkin', points:10},
+      {time: new Date(60*60*24*365*40), type: 'checkin', points:30},
+      {time: new Date("2011-01-05 14:45"), type: 'checkin', points:10},
+    ]
+  }], function(success) {
+    if (success) {
+      res.send('OK');
+    } else {
+      res.send('FAIL');
+    }
+  });
+  //datamodel.addEvent({name:"Moms", address: "Kauniaistentie 7"});
+  //datamodel.addEvent({name:"Kotipizza", address: "Kauniaistentie 7"});
+  //datamodel.addEvent({name:"Kotipizza", address: "Kauniaistentie 7"});
+});
+
+app.get('/api/venues/del/:name', function(req, res) {
+  
 });
 
 app.get('/api/db_test', function(req, res){
@@ -120,16 +204,22 @@ app.get('/api/venues', function(req, res){
 	});
 });
 
-app.get('/api/venues2', function(req, res) {
+app.get('/api/venues2/:name', function(req, res) {
   var venues = [];
-  datamodel.models.Venue.find({}, function(err, venuedata) {
-    for (i in venuedata) {
+  datamodel.getVenues({name: req.params.name}, function(venuedata) {
+    for (var i in venuedata) {
+      venues.push(venuedata[i]);
+    }
+    res.send(venues);
+  });
+  /*datamodel.models.Venue.find({}, function(err, venuedata) {
+    for (var i in venuedata) {
       console.log("\n\nVenue:\n")
       console.log(venuedata[i].doc);
       venues.push(venuedata[i].doc);
     }
     res.send(venues);
-  });
+  });*/
 });
 
 /**
